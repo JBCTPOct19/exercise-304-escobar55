df8cd41f2969fc482af84b47c819a3364c8bc8f4
Soheila Escobar <escobar5@umbc.edu>
2019-10-10 11:32:28 -0400

    Exercise_304

---
+++ src/main/java/com/example/demo/DataLoader.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataLoader implements CommandLineRunner {
+
+    @Autowired
+    CarRepository repository;
+
+    @Override
+    public void run(String... strings) throws Exception {
+        Car car;
+        car = new Car("Toyota", "Corolla", "2019");
+        repository.save(car);
+
+        car = new Car("Chevrolet", "Spark", "2020");
+        repository.save(car);
+
+        car = new Car("Hyundai", "Elantra", "2020");
+        repository.save(car);
+
+    }
+}
